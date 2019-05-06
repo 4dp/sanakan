f64cd2932089870461867304602290d0702d00d1
Konrad <sniku13@gmail.com>
2019-05-06 11:45:12 +0200

    new episodes

---
+++ src/Extensions/INewEpisodeExtension.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Discord;
+using Shinden.Models;
+
+namespace Sanakan.Extensions
+{
+    public static class INewEpisodeExtension
+    {
+        public static Embed ToEmbed(this INewEpisode ep)
+        {
+            return new EmbedBuilder()
+            {
+                Title = ep.AnimeTitle.TrimToLength(EmbedBuilder.MaxTitleLength),
+                ThumbnailUrl = ep.AnimeCoverUrl,
+                Color = EMType.Info.Color(),
+                Fields = ep.GetFields(),
+                Url = ep.AnimeUrl,
+            }.Build();
+        }
+
+        public static string ToName(this Language lang)
+        {
+            switch (lang)
+            {
+                case Language.English: return "Angielski";
+                case Language.Korean: return "Koreański";
+                case Language.Chinese: return "Chiński";
+                case Language.Polish: return "Polski";
+
+                case Language.NotSpecified:
+                default: return "--";
+            }
+        }
+
+        public static List<EmbedFieldBuilder> GetFields(this INewEpisode ep)
+        {
+            return new List<EmbedFieldBuilder>
+            {
+                new EmbedFieldBuilder
+                {
+                    Name = "Numer epizodu",
+                    Value = ep.EpisodeNumber,
+                    IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                     Name = "Czas trwania",
+                     Value = ep.EpisodeLength,
+                     IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Język napisów",
+                    Value = ep.SubtitlesLanguage.ToName(),
+                    IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Link",
+                    Value = ep.EpisodeUrl,
+                    IsInline = false
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Data dodania",
+                    Value = ep.AddDate.ToShortDateString(),
+                    IsInline = false
+                }
+            };
+        }
+    }
+}
---
+++ src/Modules/Shinden.cs
@@ -0,0 +1,59 @@
+﻿#pragma warning disable 1591
+
+using Discord.Commands;
+using Sanakan.Extensions;
+using Sanakan.Preconditions;
+using Sanakan.Services.Commands;
+using Shinden;
+using System;
+using System.Threading.Tasks;
+
+namespace Sanakan.Modules
+{
+    [Name("Shinden"), RequireCommandChannel, RequireUserRole]
+    public class Shinden : SanakanModuleBase<SocketCommandContext>
+    {
+        private ShindenClient _shclient;
+
+        public Shinden(ShindenClient client)
+        {
+            _shclient = client;
+        }
+
+        [Command("odcinki", RunMode = RunMode.Async)]
+        [Alias("episodes")]
+        [Summary("wyświetla nowo dodane epizody")]
+        [Remarks("")]
+        public async Task ShowNewEpisodesAsync()
+        {
+            var response = await _shclient.GetNewEpisodesAsync();
+            if (response.IsSuccessStatusCode())
+            {
+                var episodes = response.Body;
+                if (episodes?.Count > 0)
+                {
+                    var msg = await ReplyAsync("", embed: "Lista poszła na PW!".ToEmbedMessage(EMType.Success).Build());
+
+                    try
+                    {
+                        var dm = await Context.User.GetOrCreateDMChannelAsync();
+                        foreach (var ep in episodes)
+                        {
+                            await dm.SendMessageAsync("", false, ep.ToEmbed());
+                            await Task.Delay(500);
+                        }
+                        await dm.CloseAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        await msg.ModifyAsync(x => x.Embed = $"{Context.User.Mention} nie udało się wyłać PW! ({ex.Message})".ToEmbedMessage(EMType.Error).Build());
+                    }
+
+                    return;
+                }
+            }
+            
+            await ReplyAsync("", embed: "Nie udało się pobrać listy odcinków.".ToEmbedMessage(EMType.Error).Build());
+        }
+    }
+}
