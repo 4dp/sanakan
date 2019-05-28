397929961043bd5173b9ed30e98386f035ed0075
Sniku <sniku13@gmail.com>
2019-05-28 18:51:51 +0200

    cts in executor

--- src/Services/Executor/SynchronizedExecutor.cs
+++ src/Services/Executor/SynchronizedExecutor.cs
@@ -14,31 +14,40 @@ namespace Sanakan.Services.Executor
         private ILogger _logger;
 
         private SemaphoreSlim _semaphore = new SemaphoreSlim(1,1);
-        private BlockingCollection<IExecutable> _queue = new BlockingCollection<IExecutable>(100);
+        private BlockingCollection<IExecutable> _queue = new BlockingCollection<IExecutable>(150);
+
+        private CancellationTokenSource _cts { get; set; }
 
         public SynchronizedExecutor(ILogger logger)
         {
+            _cts = new CancellationTokenSource();
             _logger = logger;
         }
         
         public void Initialize(IServiceProvider provider)
         {
             _provider = provider;
-            
             RunWorker();
         }
 
         public bool TryAdd(IExecutable task, TimeSpan timeout) 
         {
+            _logger.Log($"Executor: qc {_queue.Count}");
             if (_queue.TryAdd(task, timeout))
             {
                 RunWorker();
                 return true;
             }
+            else
+            {
+                _cts.Cancel();
+                _cts = new CancellationTokenSource();
+                RunWorker();
+            }
             return false;
         }
 
-        public void RunWorker() => _ = Task.Run(async () => await ProcessCommandsAsync());
+        public void RunWorker() => _ = Task.Run(async () => await ProcessCommandsAsync(), _cts.Token);
 
         private async Task ProcessCommandsAsync()
         {
@@ -49,7 +58,7 @@ namespace Sanakan.Services.Executor
             {
                 while (_queue.Count > 0)
                 {
-                    if (_queue.TryTake(out var cmd, 50))
+                    if (_queue.TryTake(out var cmd, 100))
                     {
                         try
                         {
@@ -59,8 +68,13 @@ namespace Sanakan.Services.Executor
                         {
                             _logger.Log($"Executor: {ex}");
                         }
+                        await Task.Delay(50);
                     }
-                    await Task.Delay(10);
+                    else
+                    {
+                        _logger.Log($"Executor: cannot take task!");
+                        await Task.Delay(10);
+                    }
                 }
             }
             finally
