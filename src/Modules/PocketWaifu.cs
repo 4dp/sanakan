193a39b589f99043fc615c8a30060f11b56ceb59
Zsuatem <mateuszvx8.96@gmail.com>
2020-10-09 15:54:24 +0200

    Filtrowanie zablokowanych w wishlist

--- src/Modules/PocketWaifu.cs
+++ src/Modules/PocketWaifu.cs
@@ -1992,7 +1992,7 @@ namespace Sanakan.Modules
         [Alias("wishlist", "zyczenia")]
         [Summary("wyświetla liste życzeń użytkownika")]
         [Remarks("Dzida"), RequireWaifuCommandChannel]
-        public async Task ShowWishlistAsync([Summary("użytkownik(opcjonalne)")]SocketGuildUser usr = null, [Summary("czy pokazać ulubione(true/false) domyślnie ukryte, wymaga podania użytkownika")]bool showFavs = false)
+        public async Task ShowWishlistAsync([Summary("użytkownik(opcjonalne)")]SocketGuildUser usr = null, [Summary("czy pokazać ulubione(true/false) domyślnie ukryte, wymaga podania użytkownika")]bool showFavs = false, [Summary("czy pokazać niewymienialne(true/false) domyślnie pokazane, wymaga podania użytkownika (aby ten filtr zadziałał należy też podać wartość dla poprzedniego)")] bool showBlocked = true)
         {
             var user = (usr ?? Context.User) as SocketGuildUser;
             if (user == null) return;
@@ -2027,6 +2027,9 @@ namespace Sanakan.Modules
 
                 if (!showFavs)
                     cards = cards.Where(x => !x.HasTag("ulubione")).ToList();
+
+                if (!showBlocked)
+                    cards = cards.Where(x => !(x.IsTradable == false)).ToList();
 
                 if (cards.Count() < 1)
                 {
