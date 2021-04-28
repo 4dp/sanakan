616f81f59ca73d0f1493832ac32e860c8cce4f92
Konrad <sniku13@gmail.com>
2021-04-28 12:56:09 +0200

    ignore the same id

--- src/Api/Controllers/UserController.cs
+++ src/Api/Controllers/UserController.cs
@@ -235,19 +235,22 @@ namespace Sanakan.Api.Controllers
                 if (db.Users.Any(x => x.Shinden == sUser.Id))
                 {
                     await "This account is already linked!".ToResponse(401).ExecuteResultAsync(ControllerContext);
-                    var oldUsers = await db.Users.AsQueryable().Where(x => x.Shinden == sUser.Id).ToListAsync();
+                    var oldUsers = await db.Users.AsQueryable().Where(x => x.Shinden == sUser.Id && x.Id != id.DiscordUserId).ToListAsync();
 
-                    var rmcs = _config.Get().RMConfig.Where(x => x.Type == RichMessageType.AdminNotify);
-                    foreach (var rmc in rmcs)
+                    if (oldUsers.Count > 0)
                     {
-                        var guild = _client.GetGuild(rmc.GuildId);
-                        if (guild == null) continue;
+                        var rmcs = _config.Get().RMConfig.Where(x => x.Type == RichMessageType.AdminNotify);
+                        foreach (var rmc in rmcs)
+                        {
+                            var guild = _client.GetGuild(rmc.GuildId);
+                            if (guild == null) continue;
 
-                        var channel = guild.GetTextChannel(rmc.ChannelId);
-                        if (channel == null) continue;
+                            var channel = guild.GetTextChannel(rmc.ChannelId);
+                            if (channel == null) continue;
 
-                        await channel.SendMessageAsync("", embed: ($"Potencjalne multikonto:\nDID: {id.DiscordUserId}\nSID: {sUser.Id}\n"
-                            + $"SN: {sUser.Name}\n\noDID: {string.Join(",", oldUsers.Select(x => x.Id))}").TrimToLength(2000).ToEmbedMessage(EMType.Error).Build());
+                            await channel.SendMessageAsync("", embed: ($"Potencjalne multikonto:\nDID: {id.DiscordUserId}\nSID: {sUser.Id}\n"
+                                + $"SN: {sUser.Name}\n\noDID: {string.Join(",", oldUsers.Select(x => x.Id))}").TrimToLength(2000).ToEmbedMessage(EMType.Error).Build());
+                        }
                     }
                     return;
                 }
