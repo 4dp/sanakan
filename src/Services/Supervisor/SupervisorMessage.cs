39c24b12bfbac3477b4f943ebb8f4d2bf32924f2
Konrad <sniku13@gmail.com>
2021-10-28 02:05:00 +0200

    new url

--- src/Services/Supervisor/SupervisorMessage.cs
+++ src/Services/Supervisor/SupervisorMessage.cs
@@ -10,7 +10,7 @@ namespace Sanakan.Services.Supervisor
     {
         private static List<string> BannableStrings = new List<string>()
         {
-            "dliscord.com/", ".gift/"
+            "dliscord.com/", ".gift/", "discorl.com/"
         };
 
         public SupervisorMessage(string content, int count = 1)
