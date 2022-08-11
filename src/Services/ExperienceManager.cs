848f8533fbbbb69ea5fc568862971d0e0e71cbd9
Konrad <sniku13@gmail.com>
2022-08-11 13:29:23 +0200

    use guild avatar in lvlup badge

--- src/Services/ExperienceManager.cs
+++ src/Services/ExperienceManager.cs
@@ -52,7 +52,7 @@ namespace Sanakan.Services
         public async Task NotifyAboutLevelAsync(SocketGuildUser user, ISocketMessageChannel channel, long level)
         {
             using (var badge = await _img.GetLevelUpBadgeAsync(user.Nickname ?? user.Username,
-                level, user.GetUserOrDefaultAvatarUrl(), user.Roles.OrderByDescending(x => x.Position).First().Color))
+                level, user.GetUserOrDefaultAvatarUrl(true), user.Roles.OrderByDescending(x => x.Position).First().Color))
             {
                 using (var badgeStream = badge.ToPngStream())
                 {
